a7a398b727aceeef427c1a16c8abb0886a042e77
hieunguyen4193 <work@hieunguyen.de>
2024-09-03 15:57:35 +0200

    update

---
+++ 01_read_data_and_add_VDJ_info.R
@@ -0,0 +1,16 @@
+gc()
+rm(list = ls())
+
+
+outdir <- "/media/hieunguyen/HNSD_mini/outdir/LK_data_analysis"
+
+path.to.main.output <- file.path(outdir, "general_outputs")
+dir.create(path.to.main.output, showWarnings = FALSE, recursive = TRUE)
+
+dataset.names <- list(
+  Dataset1 = "1stExp_Kopplin",
+  Dataset2 = "211227_Kopplin",
+  Dataset3 = "230215_Kopplin_Pabst_added_NC_000001_merged_zcat_m330_m331_remove_c9",
+  Dataset4 = "230316_Kopplin"
+)
+
