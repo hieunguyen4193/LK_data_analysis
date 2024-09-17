814f3ba6b1386ab1bace61647387921607e95ce0
hieunguyen4193 <work@hieunguyen.de>
2024-09-17 21:02:21 +0200

    update

--- Dataset1/clusters_to_be_removed.R
+++ Dataset1/clusters_to_be_removed.R
@@ -1,11 +1,11 @@
 remove.clusters <- 
   list(
-    CD45exp1_m1_1st =  c(4, 5, 6),
-    CD45exp1_m2_1st = c(4, 5, 6, 7),
-    GFPexp1_m1_1st = c(3, 4, 5),
-    GFPexp1_m2_1st = c(3, 4, 5),
-    CD45exp1_m1_2nd = c(3, 4, 5),
-    CD45exp1_m2_2nd = c(3),
-    GFPexp1_m1_2nd = c(),
-    GFPexp1_m2_2nd = c()
+    CD45exp1_m1_1st_round =  c(4, 5, 6),
+    CD45exp1_m2_1st_round = c(4, 5, 6, 7),
+    GFPexp1_m1_1st_round = c(3, 4, 5),
+    GFPexp1_m2_1st_round = c(3, 4, 5),
+    CD45exp1_m1_2nd_round = c(3, 4, 5),
+    CD45exp1_m2_2nd_round = c(3),
+    GFPexp1_m1_2nd_round = c(),
+    GFPexp1_m2_2nd_round = c()
   ) 
