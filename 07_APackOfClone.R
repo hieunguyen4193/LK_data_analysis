0a664a00b95a97a73c33a0909f491cd1f5b44025
hieunguyen4193 <work@hieunguyen.de>
2024-11-26 15:05:13 +0100

    update

--- 07_APackOfClone.R
+++ 07_APackOfClone.R
@@ -66,62 +66,15 @@ module.gene.list <- list(
   Proliferating = c("Mki67", "Stmn1", "Top2a", "Birc5")
 )
 
-
-for (dataset.name in names(all.s.obj)){
-  if (dataset.name == "Dataset1"){
-    reduction.name <- "RNA_UMAP"
-  } else {
-    reduction.name <- "INTE_UMAP"
-  }
-  dir.create(file.path(path.to.06.output, dataset.name), showWarnings = FALSE, recursive = TRUE)
-  
-  s.obj <- all.s.obj[[dataset.name]]
-  for (input.list in names(module.gene.list)){
-    DefaultAssay(s.obj) <- "RNA"
-    s.obj <- AddModuleScore(object = s.obj, features = list(module.gene.list[[input.list]]), name = sprintf("%s_", input.list), ctrl = 50)
-  }
-  
-  meta.data <- s.obj@meta.data
-  fake.module.gene.list <- to_vec(
-    for (item in names(module.gene.list)){
-      sprintf("%s_1", item)
-    }
-  )
-  library(viridis)
-  if ("svglite" %in% installed.packages() == FALSE){
-    install.packages("svglite")
-  }
-  
-  feature.plot <- FeaturePlot(object = s.obj, reduction = reduction.name, ncol = 3, features = fake.module.gene.list, pt.size = 1) &
-    scale_color_gradient(low = "lightgray", high = "#FF0000", na.value = "lightgray")
-  
-  violin.plot <- VlnPlot(object = s.obj, features = fake.module.gene.list, pt.size = 0)
-  
-  heatmapdf <- s.obj@meta.data[, c("seurat_clusters", fake.module.gene.list)] %>%
-    group_by(seurat_clusters) %>%
-    summarise(across(everything(), mean)) %>%
-    column_to_rownames("seurat_clusters")
-  
-  colors <- rev(RColorBrewer::brewer.pal(n = length(fake.module.gene.list), name = "RdBu"))
-  colors.use <- grDevices::colorRampPalette(colors = colors)(100)
-  
-  heatmapdf.scaled <- (heatmapdf - rowMeans(heatmapdf))/rowSds(as.matrix(heatmapdf))
-  colnames(heatmapdf.scaled) <- to_vec(
-    for (item in colnames(heatmapdf.scaled)){
-      str_split(item, "_")[[1]][[1]]
-    }
-  )
-  heatmap.plot <- heatmapdf.scaled %>% rownames_to_column("cluster") %>% 
-    pivot_longer(!cluster, names_to = "signature", values_to = "z_score") %>%
-    ggplot(aes(x = cluster, y = signature, fill = z_score)) + geom_tile() + 
-    scale_fill_distiller(palette = "RdBu") + 
-    theme(axis.text = element_text(size = 22))
-  
-  ggsave(plot = feature.plot, filename = sprintf("feature_plot_module_scores.svg"), 
-         path = file.path(path.to.06.output, dataset.name), device = "svg", width = 20, height = 14)
-  ggsave(plot = violin.plot, filename = sprintf("violin_plot_module_scores.svg"), 
-         path = file.path(path.to.06.output, dataset.name), device = "svg", width = 20, height = 14)
-  ggsave(plot = heatmap.plot, filename = sprintf("heatmap_module_scores.svg"), 
-         path = file.path(path.to.06.output, dataset.name), device = "svg", width = 10, height = 10)
+dataset.name <- "Dataset2"
+if (dataset.name == "Dataset1"){
+  reduction.name <- "RNA_UMAP"
+} else {
+  reduction.name <- "INTE_UMAP"
 }
+dir.create(file.path(path.to.06.output, dataset.name), showWarnings = FALSE, recursive = TRUE)
 
+s.obj <- all.s.obj[[dataset.name]]
+
+##### install APackOfTheClone package
+install.packages("APackOfTheClones")
